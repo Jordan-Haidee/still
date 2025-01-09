3657bfc26ff71779c21ae9384f7f324a3a7ef52b
jiadonghe007 <jiadonghe007@outlook.com>
2025-01-09 21:31:24 +0800

    init repo

---
+++ src/core.rs
@@ -0,0 +1,93 @@
+#[cfg(target_os = "linux")]
+use libc::{kill, pid_t, SIGCONT, SIGSTOP};
+#[cfg(target_os = "windows")]
+use ntapi::ntpsapi::{NtResumeProcess, NtSuspendProcess};
+#[cfg(target_os = "windows")]
+use std::env::args;
+#[cfg(target_os = "windows")]
+use winapi::{
+    shared::ntdef::NULL,
+    um::{processthreadsapi::OpenProcess, winnt::PROCESS_SUSPEND_RESUME},
+};
+
+#[cfg(target_os = "windows")]
+fn suspend_process(pid: u32) -> bool {
+    let process_handle = unsafe { OpenProcess(PROCESS_SUSPEND_RESUME, 0, pid) };
+    if process_handle == NULL {
+        return false;
+    }
+    let suspend_status = unsafe { NtSuspendProcess(process_handle) };
+    if suspend_status != 0 {
+        return false;
+    }
+    println!("Process {} has been suspended successfully!", pid);
+    return true;
+}
+
+#[cfg(target_os = "windows")]
+fn resume_process(pid: u32) -> bool {
+    let process_handle = unsafe { OpenProcess(PROCESS_SUSPEND_RESUME, 0, pid) };
+    dbg!(process_handle);
+
+    if process_handle == NULL {
+        return false;
+    }
+    let suspend_status = unsafe { NtResumeProcess(process_handle) };
+    dbg!(suspend_status);
+    if suspend_status != 0 {
+        return false;
+    }
+    println!("进程 {} 已成功恢复", pid);
+    return true;
+}
+
+#[cfg(target_os = "linux")]
+fn suspend_process(pid: u32) -> bool {
+    let result = unsafe { kill(pid as pid_t, SIGSTOP) };
+    result == 0
+}
+#[cfg(target_os = "linux")]
+fn resume_process(pid: u32) -> bool {
+    let result = unsafe { kill(pid as pid_t, SIGCONT) };
+    result == 0
+}
+
+pub fn parse_duration(s: &str) -> Option<u64> {
+    if s.is_empty() {
+        return None;
+    }
+    if let Some(last_char) = s.chars().last() {
+        match last_char {
+            'h' | 'H' => {
+                let num = s[..s.len() - 1].parse::<f64>().ok()?;
+                Some((num * 3600.0) as u64)
+            }
+            'm' | 'M' => {
+                let num = s[..s.len() - 1].parse::<f64>().ok()?;
+                Some((num * 60.0) as u64)
+            }
+            's' | 'S' => {
+                let num = s[..s.len() - 1].parse::<f64>().ok()?;
+                Some(num as u64)
+            }
+            _ => s.parse::<u64>().ok(),
+        }
+    } else {
+        None
+    }
+}
+
+pub fn still(pid: u32, seconds: u64) {
+    suspend_process(pid);
+    let duration = std::time::Duration::from_secs(seconds);
+    std::thread::sleep(duration);
+    resume_process(pid);
+}
+
+#[cfg(target_os = "windows")]
+fn main() {
+    let args: Vec<String> = args().collect();
+    let pid = args[1].parse::<u32>().expect("输入pid错误！");
+    let secs = parse_duration(args[2].as_str()).expect("解析时间错误！");
+    still(pid, secs);
+}
---
+++ src/main.rs
@@ -0,0 +1,63 @@
+#[cfg(target_os = "windows")]
+use std::env::args;
+#[cfg(target_os = "windows")]
+use std::os::windows::process::CommandExt;
+#[cfg(target_os = "windows")]
+use std::process::{Command, Stdio};
+
+#[cfg(target_os = "linux")]
+use daemonize::Daemonize;
+
+#[cfg(target_os = "linux")]
+use std::fs::{self, File};
+#[cfg(target_os = "linux")]
+use std::os::unix::fs::PermissionsExt;
+mod core;
+
+#[cfg(target_os = "windows")]
+fn main() {
+    const CREATE_NEW_PROCESS_GROUP: u32 = 0x00000200;
+    const DETACHED_PROCESS: u32 = 0x00000008;
+    const CREATE_NO_WINDOW: u32 = 0x08000000;
+
+    let args: Vec<String> = args().collect();
+    let child = Command::new("target/release/core.exe")
+        .arg(args[1].as_str())
+        .arg(args[2].as_str())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .creation_flags(CREATE_NEW_PROCESS_GROUP)
+        .creation_flags(DETACHED_PROCESS)
+        .creation_flags(CREATE_NO_WINDOW)
+        .spawn()
+        .expect("Failed to spawn child process");
+    println!("Child process ID: {}", child.id());
+}
+
+#[cfg(target_os = "linux")]
+fn main() {
+    let daemon = Daemonize::new()
+        .pid_file("/tmp/daemon.pid")
+        .chown_pid_file(true)
+        .working_directory(".")
+        .umask(0o644)
+        .stdout(File::create("/tmp/daemon.out").expect("failed to create file!"))
+        .stderr(File::create("/tmp/daemon.err").expect("failed to create file!"));
+
+    let permissions = fs::Permissions::from_mode(0o644);
+    fs::set_permissions("/tmp/daemon.pid", permissions)
+        .expect("no permission to modify file authority!");
+
+    match daemon.start() {
+        Ok(_) => {
+            let args: Vec<String> = std::env::args().collect();
+            // 在后台执行某个命令
+            let pid: u32 = args[1].parse().expect("pid必须是正整数");
+            let duration: u64 = core::parse_duration(args[2].as_str()).unwrap();
+            core::still(pid, duration)
+        }
+        Err(e) => {
+            eprintln!("启动守护进程失败: {}", e);
+        }
+    }
+}
